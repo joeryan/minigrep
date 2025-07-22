4941e62933bf1a975a334d018faed8035b87d8bf
Joe Ryan <ryanjos2@msu.edu>
2025-07-22 15:31:51 -0400

    first test

--- src/lib.rs
+++ src/lib.rs
@@ -23,3 +23,23 @@ impl Config {
         Ok(Config {query, file_path} )
     }
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec!["safe, fast, productive."]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+
+    #[test]
+    fn one_result(){
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
